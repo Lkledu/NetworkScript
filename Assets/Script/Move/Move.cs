90fc92084e2af32a4205d7d4f7dac9ba5afcbc4b
Eduardo <lkledu@gmail.com>
2017-08-29 17:08:09 -0300

    testes de controle e rede

---
+++ Assets/Script/Move/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public VirtualJoystick joystick;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			Touch toque = Input.GetTouch (0);
+			Debug.Log (toque.position.x);
+
+
+			joystick.Around ();
+			/*
+			if(toque.position.x > (Screen.width/2))
+				transform.RotateAround (Vector3.zero, Vector3.up, Time.deltaTime * -20);
+			if(toque.position.x < (Screen.width/2))
+				transform.RotateAround (Vector3.zero, Vector3.up, Time.deltaTime * 20);
+		*/
+		}
+	}		
+}
